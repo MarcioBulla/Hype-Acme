cde2d5cc2c6b3bdcd35270d337c8b14cd0bc5dae
Marcio Bulla <marciobulla@ufpr.br>
2022-11-07 02:06:04 -0300

    COMPLETO

--- import_data.py
+++ import_data.py
@@ -1,20 +1,40 @@
 import mysql.connector
 import pandas as pd
 from sqlalchemy import create_engine
-from sqlalchemy.types import Integer, Boolean, Date, VARCHAR
+from sqlalchemy.types import Integer, Boolean, Date, VARCHAR, Float
 from sqlalchemy.schema import Table, MetaData, Column, ForeignKey
 import os
+import json
 
-host = "localhost"
+host = "mydb"
 
+print("Preparando dados...")
 path_ipca = os.path.join("data", "ipca.json")
 
 path_exec = os.path.join("data", "executions.csv")
 path_func = os.path.join("data", "functions.csv")
 
+file_ipca = open(path_ipca)
+json_ipca = json.load(file_ipca)
+file_ipca.close()
+
+for entities in json_ipca:
+    for elements in entities['resultados']:
+        for var in elements['series']:
+            ipca_var = var['serie']
+
+ano_mes, buying_power = [], []
+for key, val in ipca_var.items():
+    ano_mes.append(key)
+    buying_power.append(val)
+
+df_icpa = pd.DataFrame({"date": ano_mes, "buying_power": buying_power})
+df_icpa["date"] = pd.to_datetime(df_icpa["date"], format="%Y%m") 
+
 df_exec = pd.read_csv(path_exec)
 df_func = pd.read_csv(path_func)
 
+print("Concluido")
 
 print("Connectando no docker...")
 db = mysql.connector.connect(
@@ -39,6 +59,10 @@ engine_1 = create_engine(
 engine_23 = create_engine(
     "mysql+mysqlconnector://root:acme@"+host+":3306/service")
 
+
+df_icpa.to_sql("ipca", con=engine_1, if_exists='replace', index=False, dtype={
+    "date": Date ,
+    "buying_power": Float()})
 
 df_func.to_sql("functions", con=engine_23, if_exists='replace', index=False, dtype={
     "id": Integer ,
