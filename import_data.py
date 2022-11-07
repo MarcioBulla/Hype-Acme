4f4c26ad9ab8526a123d3f5189f56be02e32fb2a
Marcio Bulla <marciobulla@ufpr.br>
2022-11-07 01:17:44 -0300

    TALBLES executions and funcitons complet

--- import_data.py
+++ import_data.py
@@ -2,87 +2,70 @@ import mysql.connector
 import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.types import Integer, Boolean, Date, VARCHAR
-from sqlalchemy.schema import Table, MetaData, Column
+from sqlalchemy.schema import Table, MetaData, Column, ForeignKey
+import os
 
-host = "mydb"
+host = "localhost"
 
-url_IPCA = "https://servicodados.ibge.gov.br/api/v3/agregados/1420/periodos/201201%7C201202%7C201203%7C201204%7C201205%7C201206%7C201207%7C201208%7C201209%7C201210%7C201211%7C201212%7C201301%7C201302%7C201303%7C201304%7C201305%7C201306%7C201307%7C201308%7C201309%7C201310%7C201311%7C201312%7C201401%7C201402%7C201403%7C201404%7C201405%7C201406%7C201407%7C201408%7C201409%7C201410%7C201411%7C201412%7C201501%7C201502%7C201503%7C201504%7C201505%7C201506%7C201507%7C201508%7C201509%7C201510%7C201511%7C201512%7C201601%7C201602%7C201603%7C201604%7C201605%7C201606%7C201607%7C201608%7C201609%7C201610%7C201611%7C201612%7C201701%7C201702%7C201703%7C201704%7C201705%7C201706%7C201707%7C201708%7C201709%7C201710%7C201711%7C201712%7C201801%7C201802%7C201803%7C201804%7C201805%7C201806%7C201807%7C201808%7C201809%7C201810%7C201811%7C201812%7C201901%7C201902%7C201903%7C201904%7C201905%7C201906%7C201907%7C201908%7C201909%7C201910%7C201911%7C201912/variaveis/306?localidades=N1%5Ball%5D&amp;classificacao=315%5B7169%5D"
+path_ipca = os.path.join("data", "ipca.json")
 
+path_exec = os.path.join("data", "executions.csv")
+path_func = os.path.join("data", "functions.csv")
 
-url_exec = "https://drive.google.com/file/d/1tQsYSIN2YXrMACxRjIDCnDTV10K4NKJ_/view?usp=sharing"
-url_exec = 'https://drive.google.com/uc?export=download&id=' + \
-    url_exec.split('/')[-2]
+df_exec = pd.read_csv(path_exec)
+df_func = pd.read_csv(path_func)
 
-url_func = "https://drive.google.com/file/d/1AJmk-B-Ok1kvSZGnXa1HlJjX5nojkQOe/view?usp=sharing"
-url_func = 'https://drive.google.com/uc?export=download&id=' + \
-    url_func.split('/')[-2]
-
-df_exec = pd.read_csv(url_exec)
-df_func = pd.read_csv(url_func)
 
 print("Connectando no docker...")
 db = mysql.connector.connect(
     host=host, port=3306,
     user='root', password='acme')
-print("Completo")
+print("connectado")
 
+
+print("Criando DATABASES...")
 db_cursor = db.cursor()
-# Criar databases
+
 db_cursor.execute('CREATE DATABASE IF NOT EXISTS service')
 db_cursor.execute('CREATE DATABASE IF NOT EXISTS ibge')
 db_cursor.close()
 db.close()
+print("Concluido")
 
 
-
+print("Criando TABLES...")
 engine_1 = create_engine(
     "mysql+mysqlconnector://root:acme@"+host+":3306/ibge")
 engine_23 = create_engine(
     "mysql+mysqlconnector://root:acme@"+host+":3306/service")
 
-cursor_1 = engine_1.connect()
-cursor_23 = engine_23.connect()
 
-
-tb_func = Table("functions", MetaData(), 
-    Column("id", Integer, primary_key=True),
-    Column("function_name", VARCHAR(20)),
-    Column("external_component_avg_latency", Integer),
-    Column("has_external_component", Boolean)).create(engine_23)
-
-tb_exec = Table("executions", MetaData(), 
-    Column("id", Integer, primary_key=True),
-    Column("date", Date),
-    Column("function_id", Integer, foreign_key=tb_func.id),
-    Column("execution_time", Integer)).create(engine_23)
-
-df_exec.to_sql("executions", con=engine_23, if_exists='append', index=False, dtype={
-    "id": Integer, 
-    "date": Date,
-    "function_id": Integer, 
-    "execution_time": Integer,})
-df_func.to_sql("functions", con=engine_23, if_exists='append', index=False, dtype={
+df_func.to_sql("functions", con=engine_23, if_exists='replace', index=False, dtype={
     "id": Integer ,
     "function_name": VARCHAR(20) ,
     "external_component_avg_latency": Integer,
     "has_external_component": Boolean})
 
+df_exec.to_sql("executions", con=engine_23, if_exists='replace', index=False, dtype={
+    "id": Integer, 
+    "date": Date,
+    "function_id": Integer, 
+    "execution_time": Integer})
 
-print("Criado")
+print("Concluido")
+print("Definindo chaves...")
 
-# db_cursor.execute("""
-# ALTER TABLE executions
-# ADD PRIMARY KEY (id)
-# """)
 
-# db_cursor.execute("""
-# ALTER TABLE functions
-# ADD PRIMARY KEY (id)
-# """)
+with engine_23.connect() as conn:
+    conn.execute("""
+    ALTER TABLE functions ADD PRIMARY KEY(id);
+    """)
 
-# db_cursor.execute("""
-# ALTER TABLE executions
-# ADD FOREIGN KEY (function_id) REFERENCES functions(id)
-# """)
+    conn.execute("""
+    ALTER TABLE executions ADD PRIMARY KEY (id)
+    """)
+    conn.execute("""
+    ALTER TABLE executions ADD FOREIGN KEY (function_id) REFERENCES functions(id)
+    """)
+
 print("Chaves definidas")
-
